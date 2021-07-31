07c171eb8bda8c929678274a5cb8521b6846873a
ThatKit <emper137137@gmail.com>
2021-07-31 15:20:29 +1000

    backwards text functionality

--- interface.js
+++ interface.js
@@ -1,4 +1,23 @@
-/* () Keyboard Controls */
+/* (1) Texts Toggling */
+
+const startText = document.getElementById('start-text');
+const backwardsText = document.getElementById('backwards-text');
+const eatenItselfText = document.getElementById('eaten-itself-text');
+const crossedBorderText = document.getElementById('crossed-border-text');
+const victoryText = document.getElementById('victory-text');
+
+const showText = textEl => {
+    let textArr = [startText, backwardsText, eatenItselfText, crossedBorderText, victoryText];
+    textArr.forEach(el => el.classList.toggle('show', false));
+    textEl.classList.add('show');
+}
+
+const showBackwardsText = () => {
+    showText(backwardsText);
+    setTimeout(() => backwardsText.classList.remove('show'), 1000);
+}
+
+/* (2) Keyboard Controls */
 
 let intervalID;
 const intervalLoop = () => {
@@ -11,21 +30,29 @@ window.addEventListener('keydown', function (event) {
         case "ArrowLeft":
             if (snakeHead.direction !== 'right') {
                 snakeHead.direction = 'left';
-            }            
+            } else {
+                showBackwardsText();
+            }
             break;
         case "ArrowRight":
             if (snakeHead.direction !== 'left') {
                 snakeHead.direction = 'right';
+            } else {
+                showBackwardsText();
             }
             break;
         case "ArrowUp":
             if (snakeHead.direction !== 'bot') {
                 snakeHead.direction = 'top';
+            } else {
+                showBackwardsText();
             }
             break;
         case "ArrowDown":
             if (snakeHead.direction !== 'top') {
                 snakeHead.direction = 'bot';
+            } else {
+                showBackwardsText();
             }
             break;
         case " ": // only for test
